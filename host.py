9cd17517b91cc03799ea0df85f930c1907b75c49
jbt <gadi212007@gmail.com>
2025-08-27 16:51:17 +0300

    gadi and breail

---
+++ host.py
@@ -0,0 +1,32 @@
+all_host_list = []
+host_list={
+    "chosen event": "",
+    "date_of_event":"",
+    "chosen_district":"",
+    "hour_of_event":""
+
+}
+
+# number_of_host=input("number of host is:")
+name_of_event = input("please enter your name of event:")
+chosen_event=input("please enter your event:")
+date_of_event=input("enter the date:")
+chosen_district = input("please enter the district:")
+hour_of_event=input("enter the hour:")
+
+
+host_list["name of event"]=name_of_event
+host_list["chosen event"]=chosen_event
+host_list["date of event"]=date_of_event
+host_list["hour of event"]=hour_of_event
+host_list["chosen district"]=chosen_district
+
+
+
+print("CONGRATULATIONS!!!!!! YOUR EVENT HAS BEEN NOTED , THE INVENTIONS HAS BEEN SEND ")
+# for i in range(len(number_of_host)):
+
+
+all_host_list.append
+
+# print(host_list)
