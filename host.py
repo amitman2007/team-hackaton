37bcf8d4cc4ed2c110181c1beb99910d089527f3
priel <prielsmay2007@gmail.com>
2025-08-28 10:32:44 +0300

    host

--- host.py
+++ host.py
@@ -1,13 +1,7 @@
 all_host_list = []
-host_list={
-    "chosen event": "",
-    "date_of_event":"",
-    "chosen_district":"",
-    "hour_of_event":""
+host_list={}
 
-}
 
-# number_of_host=input("number of host is:")
 name_of_event = input("please enter your name of event:")
 chosen_event=input("please enter your event:")
 date_of_event=input("enter the date:")
@@ -15,18 +9,14 @@ chosen_district = input("please enter the district:")
 hour_of_event=input("enter the hour:")
 
 
-host_list["name of event"]=name_of_event
+host_list["name_of_event"]=name_of_event
 host_list["chosen event"]=chosen_event
 host_list["date of event"]=date_of_event
-host_list["hour of event"]=hour_of_event
 host_list["chosen district"]=chosen_district
+host_list["hour of event"]=hour_of_event
+all_host_list.append(host_list)
 
 
 
 print("CONGRATULATIONS!!!!!! YOUR EVENT HAS BEEN NOTED , THE INVENTIONS HAS BEEN SEND ")
-# for i in range(len(number_of_host)):
-
-
-all_host_list.append
-
-# print(host_list)
+print(all_host_list)
--- hosted.py
+++ hosted.py
@@ -1,33 +1,41 @@
 hosted_list=[]
 
 
-# events_list=["holiday eve", "lodging", "visit elderly people","happy events"]
-# print("welcome to de website our events: ", events_list)
-#
-#
-# def event_sort():
-#
-#     choosen_event=input("please enter your event:")
-#     for i in range(len(events_list)):
-#         if choosen_event == events_list[i]:
-#             hosted_list.append(choosen_event)
-#             print("searching events for you by name:",events_list[i])
-#             # print(hosted_list)
-# event_sort()
-#
-# date_of_event=input("enter the date:")
-# hosted_list.append(date_of_event)
+events_list=["holiday eve", "lodging", "visit elderly people","happy events"]
+print("welcome to de website our events: ", events_list)
 
+
+def event_sort():
+
+    choosen_event=input("please enter your event:")
+    for i in range(len(events_list)):
+        if choosen_event == events_list[i]:
+            hosted_list.append(events_list[i])
+            # print(hosted_list)
+event_sort()
+
+
+date_of_event=input("enter the date:")
+hosted_list.append(date_of_event)
+
+district = [
+        "jerusalem district", "northern district ", "haifa district",
+            "central district", "south district", "west district"
+
+            ]
+print(" chose your district from the list: ", district)
 
 def district_sort ():
-    district = ["jerusalem district", "northern district ", "haifa district", "central district", "south district",
-                "west district"]
+
     chosen_district = input("please enter the district:")
     for i in range(len(district)):
         if chosen_district == district[i]:
-            hosted_list.append(chosen_district)
-            print("searching events for you by name:", chosen_district[i])
+            hosted_list.append(district[i])
             print(hosted_list)
 district_sort()
 
-# print(hosted_list)
+
+
+
+
+
