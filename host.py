4961d37c8afe6c51de3678ebba513665e35e0af7
priel <prielsmay2007@gmail.com>
2025-08-28 11:03:06 +0300

    hosted/host update

--- host.py
+++ host.py
@@ -3,20 +3,20 @@ host_list={}
 
 def the_host():
     name_of_event = input("please enter your name of event:")
-    events_list=["holiday eve", "lodging", "visit elderly people","happy events"]
+    events_list={"holiday eve":1, "lodging":2, "visit elderly people":3,"happy events":4}
     print("welcome to de website our events: ", events_list)
 
 
-    chosen_event=input("please enter your event:")
+    chosen_event=input("please enter the number of the event(1-4):")
     date_of_event=input("enter the date:")
-    district = [
-            "jerusalem district", "northern district ", "haifa district",
-                "central district", "south district", "west district"
+    district = {
+            "jerusalem district":1, "northern district ":2, "haifa district":3,
+                "central district":4, "south district":5, "west district":6
 
-                ]
+    }
     print(" chose your district from the list: ", district)
 
-    chosen_district = input("please enter the district:")
+    chosen_district = input("please enter the num of the district(1-6):")
     hour_of_event=input("enter the hour:")
 
 
--- hosted.py
+++ hosted.py
@@ -1,13 +1,13 @@
 hosted_list=[]
 
 
-events_list=["holiday eve", "lodging", "visit elderly people","happy events"]
+events_list={"holiday eve":1, "lodging":2, "visit elderly people":3,"happy events":4}
 print("welcome to de website our events: ", events_list)
 
 
 def event_sort():
 
-    choosen_event=input("please enter your event:")
+    choosen_event=input("please enter the number of your event(1-4):")
     for i in range(len(events_list)):
         if choosen_event == events_list[i]:
             hosted_list.append(events_list[i])
@@ -22,16 +22,16 @@ def date_of_party():
 
 date_of_party()
 
-district = [
-        "jerusalem district", "northern district ", "haifa district",
-            "central district", "south district", "west district"
+district = {
+        "jerusalem district":1, "northern district ":2, "haifa district":3,
+            "central district":4, "south district":5, "west district":6
 
-            ]
+}
 print(" chose your district from the list: ", district)
 
 def district_sort ():
 
-    chosen_district = input("please enter the district:")
+    chosen_district = input("please enter the number of the chosen district(1-6):")
     for i in range(len(district)):
         if chosen_district == district[i]:
             hosted_list.append(district[i])
