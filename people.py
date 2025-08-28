ec509e2318300e462663456961c4cec98f58be9d
priel <prielsmay2007@gmail.com>
2025-08-28 14:36:14 +0300

    hosted/host update

--- people.py
+++ people.py
@@ -1,21 +1,29 @@
 import random
+import copy
+from const import user_database
+
 people_list=[]
 people_dict={}
+events_list_choice =  ["holiday", "kiddush",  "meetup",  "other events"]
+district_choice = ["jerusalem district",  "northern district ",  "haifa district",
+                   "central district", "south district", "west district"]
+user_to_add=[]
 def rand_people():
    people_list=[]
    people_dict={}
    for i in range(100):
-    username=(f"person{i+1}")
-    events_list_choice =  ["holiday", "kiddush",  "meetup",  "other events"]
-    district_choice = ["jerusalem district",  "northern district ",  "haifa district",
-     "central district", "south district", "west district"]
-    event= random.choice(0,3)
-    living=random.choice(0,5)
+       username = (f"person{i + 1}")
 
-    people_list.append(events_list_choice[event],district_choice[living])
-    people_dict[username]=people_list
+       event = random.randrange(0, 3)
+       living = random.randrange(0, 5)
+
+       user_to_add.append(events_list_choice[event])
+       user_to_add.append(district_choice[living])
+
+       people_dict[username] = copy.deepcopy(user_to_add)
+       user_to_add.clear()
    return people_dict
-   print(rand_people())
+
+print(rand_people())
 
 
-rand_people()
